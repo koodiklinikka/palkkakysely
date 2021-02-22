511b2da2f38dea32fff1aff88d56901c36452f44
Aarni Koskela <akx@iki.fi>
2021-02-22 14:40:26 +0200

    Add React analysaattori

---
+++ analysaattori/src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PivotTableUI from 'react-pivottable/PivotTableUI';
+import 'react-pivottable/pivottable.css';
+import TableRenderers from 'react-pivottable/TableRenderers';
+import createPlotlyComponent from 'react-plotly.js/factory';
+import createPlotlyRenderers from 'react-pivottable/PlotlyRenderers';
+import useSWR from "swr/esm";
+
+const Plot = createPlotlyComponent(window.Plotly);
+const PlotlyRenderers = createPlotlyRenderers(Plot);
+const renderers = Object.assign({}, TableRenderers, PlotlyRenderers);
+
+
+function App() {
+  const qs = new URLSearchParams(window.location.search);
+  const [pivotState, setPivotState] = React.useState({});
+  const dataSwr = useSWR(qs.get("url") || "/palkkakysely/data.json");
+  if (!dataSwr.data) {
+    if (dataSwr.error) {
+      return <>Virhe ladatessa dataa: {`${dataSwr.error}`}</>;
+    }
+    return <>Ladataan...</>;
+  }
+  return (
+    <div>
+      <PivotTableUI
+        data={dataSwr.data}
+        renderers={renderers}
+        onChange={setPivotState}
+        {...pivotState}
+      />
+    </div>
+  );
+}
+
+export default App;
