767c9a8ed69f8bb1f246652736cfd5bba2f673af
Aarni Koskela <akx@iki.fi>
2023-09-28 15:28:42 +0300

    Run prettier

--- analysaattori/src/App.tsx
+++ analysaattori/src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import PivotTableUI from 'react-pivottable/PivotTableUI';
-import 'react-pivottable/pivottable.css';
-import TableRenderers from 'react-pivottable/TableRenderers';
-import createPlotlyComponent from 'react-plotly.js/factory';
-import createPlotlyRenderers from 'react-pivottable/PlotlyRenderers';
+import React from "react";
+import PivotTableUI from "react-pivottable/PivotTableUI";
+import "react-pivottable/pivottable.css";
+import TableRenderers from "react-pivottable/TableRenderers";
+import createPlotlyComponent from "react-plotly.js/factory";
+import createPlotlyRenderers from "react-pivottable/PlotlyRenderers";
 import useSWR from "swr/esm";
 
 const Plot = createPlotlyComponent(window.Plotly);
 const PlotlyRenderers = createPlotlyRenderers(Plot);
 const renderers = Object.assign({}, TableRenderers, PlotlyRenderers);
-
 
 function App() {
   const qs = new URLSearchParams(window.location.search);
